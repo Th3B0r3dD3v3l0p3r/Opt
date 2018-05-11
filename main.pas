8fa21775ca5acdb69685c3af7d6a3c5bd7861161
Th3B0r3dD3v3l0p3r <franciszek.witt@protonmail.com>
2018-05-11 23:13:47 +0200

    Updated

--- main.pas
+++ main.pas
@@ -50,8 +50,9 @@ begin
 for j:=1 to m do
 begin
 
-if(w[i-1,j]=w[i,j-1]) then w[i,j]:=2*p[i-1,j]
-else w[i,j]:=max(p[i-1,j],p[i,j-1]);
+if(p[i-1,j]=p[i,j-1]) then w[i,j]:=2*p[i-1,j]
+else if((p[i-1,j]>p[i,j-1])) then w[i,j]:=[i-1,j]
+else w[i,j]:=[i,j-1];
 
 
 end;
