3c995871944aefa6ac4464d4e32ad6b88dc7cb8e
Th3B0r3dD3v3l0p3r <franciszek.witt@protonmail.com>
2018-05-12 00:07:15 +0200

    Updated

--- main.pas
+++ main.pas
@@ -2,71 +2,79 @@ function max(a,b:Qword):qword;
 begin
 if a>b then max:=a
 else max:=b;
+
+
 end;
 
+
+
 var
-n,m,i,j:word;
-z:char;
-t,p,l:array [0..1001,0..1001] of Qword;
+t,p,w:array [0..1001,0..1001] of qword;
+m,n,i,j:word;
+modv:qword;
+buff:char;
 begin
-    readln(n,m);
-    for i:=1 to n do
-    begin
-    for j:=1 to m+1 do
-    begin
-    read(z);
-    if j<=m then
-    begin
-    if z='*' then t[i,j]:=1;
-    end;
-    end;
-    end;
+modv:=1000000009;
+readln(n,m);
+for i:=1 to n do
+begin
+for j:=1 to m+1 do
+begin
+read(buff);
+if j<=m then
+begin
+if buff='*' then t[i,j]:=1;
+//write(t[i,j],' ',i,' ',j,'   ');
+end;
+end;
+end;
 
-    p[0][0]:=t[0][0];
-    p[0][0]:=t[0][0];
-    l[0][0]:=1;
-    l[0][0]:=1;
-  
-    for i:=1 to n-1 do
-    begin
-        p[i][0]:=p[i-1][0]+t[i][0];
-        l[i][0]:=1;
-    end;
-    for i:=1 to m-1 do
-    begin
-        p[0][i]:=p[0][i-1]+t[0][i];
-        l[0][i]:=1;
-    end;
-    for  i:=1 to n-1 do
-    begin
-        for j:=1 to m-1 do
-        begin
-            p[i][j]:=max(p[i-1][j], p[i][j-1])+t[i][j];
-            if(p[i-1][j]=p[i][j-1]) then
-            begin
-                l[i][j]:=(l[i-1][j] mod 1000000009+l[i][j-1] mod 1000000009) mod 1000000009;
-            end
-            else
-            begin
-                if(p[i-1][j]>p[i][j-1]) then
-                begin
-                    l[i][j]:=l[i-1][j];
-                end
-                else
-                begin
-                    if(p[i-1][j]<p[i][j-1]) then
-                    begin
-                        l[i][j]:=l[i][j-1];
-                    end;
-                end;
-            end;
-        end;
-    end;
+for i:=1 to n do
+begin
+for j:=1 to m do
+begin
+p[i][j]:=p[i][j]+max(p[i][j-1],p[i-1][j])+t[i][j];
 
-writeln(l[n-1,m-1]);
-  end.
+end;
+end;
 
 
 
 
 
+
+
+
+for i:=1 to n do
+begin
+for j:=1 to m do
+begin
+
+if(p[i-1,j]=p[i,j-1]) then w[i,j]:=2*p[i-1,j]
+else if(p[i-1,j]>p[i,j-1]) then w[i,j]:=p[i-1,j]
+else w[i,j]:=p[i,j-1];
+
+
+end;
+end;
+
+
+
+
+
+
+
+
+
+
+
+for i:=1 to n do
+begin
+for j:=1 to m do
+begin
+write(p[i,j]);
+end;
+writeln();
+end;
+
+end.
