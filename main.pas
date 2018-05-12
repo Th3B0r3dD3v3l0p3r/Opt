37d506d0d5021edbca60a461d237a7f814350703
Th3B0r3dD3v3l0p3r <franciszek.witt@protonmail.com>
2018-05-12 18:39:46 +0200

    Updated

--- main.pas
+++ main.pas
@@ -1,58 +1,47 @@
-
 var
 n,m,a,i,j,modv:longword;
-t1,t2:array [0..1000,0..1000] of longword;
+tab1,tab2:array [0..1000,0..1000] of longword;
 buff:char;
 begin
-modv:=1000000009;
-    readln(n,m);
-    
+readln(n,m);
+modv:=1000000000+9;
 for i:=0 to n-1 do
 begin
 for j:=0 to m-1 do
 begin
-buff:='a';
-while ((buff<>'*') and (buff<>'.')) do read(buff);
-if buff='*' then t1[i,j]:=1;
-//write(t[i,j],' ',i,' ',j,'   ');
+buff:='F';
+while ((buff<>'.') and (buff<>'*')) do read(buff);
+if buff<>'.' then tab1[i,j]:=1;
 end;
 end;
-
-
-
-
-
-    for i:=1 to n-1 do  begin
-    t1[i][0]:=t1[i][0]+t1[i-1][0]; 
-    t2[i][0]:=1;
-    end;
-
-
-
-    for i:=1 to m-1 do
-    begin
-    t1[0][i]:=t1[0][i]+t1[0][i-1];
-    t2[0][i]:=1;
-    end;
-    for i:=1 to n-1  do
-        begin
-        for j:=1 to m-1 do
-            begin
-            if (t1[i-1][j]=t1[i][j-1]) then
-                begin
-                t1[i][j]:=t1[i][j]+t1[i-1][j];
-                t2[i][j]:=(t2[i-1][j]+t2[i][j-1]) mod modv;
-                end
-                 else if (t1[i-1][j]>t1[i][j-1]) then
-                begin
-                t1[i][j]:=t1[i][j]+t1[i-1][j];
-                t2[i][j]:=t2[i-1][j];
-                end
-                else begin
-                t1[i][j]:=t1[i][j]+t1[i][j-1];
-                t2[i][j]:=t2[i][j-1];
-            end;
-        end;
-    end;
-    writeln(t2[n-1][m-1]);
+for i:=1 to n-1 do  begin
+tab1[i][0]:=tab1[i][0]+tab1[i-1][0]; 
+tab2[i][0]:=1;
+end;
+for i:=1 to m-1 do
+begin
+tab1[0][i]:=tab1[0][i]+tab1[0][i-1];
+tab2[0][i]:=1;
+end;
+for i:=1 to n-1  do
+begin
+for j:=1 to m-1 do
+begin
+if (tab1[i-1][j]=tab1[i][j-1]) then
+begin
+tab1[i][j]:=tab1[i][j]+tab1[i-1][j];
+tab2[i][j]:=(tab2[i-1][j]+tab2[i][j-1]) mod modv;
+end
+else if (tab1[i-1][j]>tab1[i][j-1]) then
+begin
+tab1[i][j]:=tab1[i][j]+tab1[i-1][j];
+tab2[i][j]:=tab2[i-1][j];
+end
+else begin
+tab1[i][j]:=tab1[i][j]+tab1[i][j-1];
+tab2[i][j]:=tab2[i][j-1];
+end;
+end;
+end;
+writeln(tab2[n-1][m-1]);
 end.
