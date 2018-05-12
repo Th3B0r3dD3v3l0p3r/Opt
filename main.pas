5147a01931de8b09da6edf366b4af5d6dcde881e
Th3B0r3dD3v3l0p3r <franciszek.witt@protonmail.com>
2018-05-12 08:05:42 +0200

    Updated

--- main.pas
+++ main.pas
@@ -4,7 +4,7 @@ buff:byte;
 n,m,k,i,j:word;
 begin
 readln(n,m);
-for i:=1 to n do for j:=1 to m do read(t[i,j]);
+for i:=1 to n do for j:=1 to m do while (t[i,j]<>'1') and (t[i,j]<>'0') do read(t[i,j]);
 
 for i:=1 to n do for j:=1 to m do write(t[i,j]);
 
