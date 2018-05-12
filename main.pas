b32e3d2af6f521edd6ac0fe934bd533705ad21f3
Th3B0r3dD3v3l0p3r <franciszek.witt@protonmail.com>
2018-05-12 08:01:22 +0200

    Updated

--- main.pas
+++ main.pas
@@ -1,11 +1,11 @@
 var
-t:array [0..501] of char;
+t:array [0..501][0..501] of char;
 buff:byte;
 n,m,k,i,j:word;
 begin
-readln(n);
-for i:=1 to n do read(t[n]);
+readln(n,m);
+for i:=1 to n do for j:=1 to m do read(t[j]);
 
-
+for i:=1 to n do for j:=1 to m do write(t[j];
 
 end.
